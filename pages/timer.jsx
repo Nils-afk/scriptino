5c46bb33570d56e1ce42fe0673edd482c8f815e8
Nils <97766170+Nils-afk@users.noreply.github.com>
2023-08-08 15:40:23 +0200

    Update timer.jsx

--- pages/timer.jsx
+++ pages/timer.jsx
@@ -1,29 +1,12 @@
-// pages/index.js
-import React, { useEffect } from 'react';
-import Head from 'next/head';
-import styles from '../styles/ComingSoon.module.css';
+// pages/timer.js
+import { useEffect } from 'react';
 
-const Home = () => {
+const TimerPage = () => {
   useEffect(() => {
-    // Redirect to the external website after a delay
-    setTimeout(() => {
-      window.location.href = 'https://coming-soon-ivory.vercel.app/';
-    }, 3000); // Delay in milliseconds (3 seconds in this example)
+    window.location.href = 'https://coming-soon-ivory.vercel.app/';
   }, []);
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Redirecting...</title>
-      </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Redirecting...</h1>
-        <p className={styles.description}>
-          You are being redirected to our website.
-        </p>
-      </main>
-    </div>
-  );
+  return null;
 };
 
-export default Home;
+export default TimerPage;
